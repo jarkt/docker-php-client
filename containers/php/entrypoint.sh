1d04f472b0616cb394740ee429db94c16c1bf476
Jark Tonnätt <j.tonnaett@snap-agentur.de>
2016-02-23 15:24:41 +0100

    format

--- containers/php/entrypoint.sh
+++ containers/php/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [ "$1" = 'install' ]
-	then
-		cd /project
-		php -r "readfile('https://getcomposer.org/installer');" | php
-		/project/composer.phar install
-	elif [ "$1" = 'update' ]
-		then
-			/project/composer.phar update
-	else
-		/project/vendor/bin/phpunit --bootstrap /project/vendor/autoload.php /project/tests/
+if [ "$1" = 'install' ]; then
+	cd /project
+	php -r "readfile('https://getcomposer.org/installer');" | php
+	/project/composer.phar install
+elif [ "$1" = 'update' ]; then
+	/project/composer.phar update
+else
+	/project/vendor/bin/phpunit --bootstrap /project/vendor/autoload.php /project/tests/
 fi
